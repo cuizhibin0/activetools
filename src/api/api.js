656373ded760f750dc4d05fd897ddac7a4943a72
cuizhibin0 <18518679659@163.com>
2018-09-04 15:51:13 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -11,11 +11,19 @@ const API = {
   ADD_MCC: '/mcc/save',
   FIND_MCC: '/mcc/find',
   FIND_MCC_BY_ID: '/mcc/findById',
-  
+
   // PAYTYPE
   ADD_PAYTYPE: '/payType/save',
   FIND_PAYTYPE: '/payType/find',
   FIND_PAYTYPE_BY_ID: '/payType/findById',
+
+  //RULE
+  ADD_RULE: '/toolRule/save',
+  ADD_RULE_DETAIL: '/toolRuleDetail/save',
+  FIND_RULE: '/toolRule/find',
+
+  FIND_RULE_DETAIL: '/toolRuleDetail/find',
+  FIND_RULE_DETAIL_BY_ID: '/toolRuleDetail/findById',
 
   IMG_UPLOAD: DVE_URL + '/file/imgUpload',
   QUERY_USER_LIST: '/user/login',
